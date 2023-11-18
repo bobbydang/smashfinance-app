5136b95560c795993cf8063b9e4681aa5280904e
Bobby Dang <bobbydang613@gmail.com>
2023-11-17 22:16:36 -0500

    Refactored angular project to not use SSR - Created CRUD for StockController

---
+++ src/main/java/com/smashfinance/controllers/StockController.java
@@ -0,0 +1,31 @@
+package com.smashfinance.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.smashfinance.entity.Stock;
+import com.smashfinance.services.StockService;
+
+
+@RestController
+@RequestMapping("/api/v1/stocks")
+public class StockController {
+
+    private StockService stockService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Stock>> getAll() {
+        return new ResponseEntity<>(stockService.findAll(), HttpStatus.OK);
+    }
+
+    @Autowired
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+
+    }
+
+}
